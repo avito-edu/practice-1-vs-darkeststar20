e23bdcfad4a0c98b654864cc7aec670b2b60f1be
darkest star <darkest_star@list.ru>
2025-09-15 22:16:23 +0300

    added calculator

--- homework/main.go
+++ homework/main.go
@@ -1,20 +1,54 @@
 package main
 
+import "fmt"
+
 func Add(a, b float64) float64 {
-	// TODO: implement me
-	return 0.0
+	return a + b
 }
 
 func Divide(a, b float64) float64 {
-	// TODO: implement me
-	// and don't forget to handle corner case
-	return 0.0
+	if a == 0 || b == 0 {
+	    panic("На ноль делить нельзя!")
+	} else {
+        //fmt.Println("На ноль делить нельзя!")
+        return a / b
+	}
 }
 
-// Same for:
-// — Subtract
-// — Multiply
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
 
 func main() {
-	// TODO: process input and implement terminal calculator
+	var operation string
+	var a, b float64
+	fmt.Print("Введите число a")
+	fmt.Scan(&a)
+	fmt.Print("Введите число b")
+	fmt.Scan(&b)
+	fmt.Print("Введите название операции")
+	_, err := fmt.Scan(&operation)
+	if err != nil {
+	    fmt.Println("Error: ", err)
+	}
+
+    fmt.Println("Вы ввели некорректное число", a, b)
+
+	switch operation {
+	case "Add":
+        fmt.Println(Add(a,b))
+    case "Divide":
+        fmt.Println(Divide(a,b))
+    case "Subtract":
+        fmt.Println(Divide(a,b))
+    case "Multiply":
+        fmt.Println(Multiply(a,b))
+    default:
+        fmt.Println("Error: Неверное название операции")
+    }
+    defer fmt.Println("Программа завершена.")
 }
