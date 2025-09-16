614a8a030a287bcf4ec5d7862c1a145e93ffd905
Anastasiya Mustafaeva <aangavrilova@avito.ru>
2025-09-16 21:26:31 +0300

    added retry

--- homework/main.go
+++ homework/main.go
@@ -1,18 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Add(a, b float64) float64 {
 	return a + b
-}
-
-func Divide(a, b float64) float64 {
-	if a == 0 || b == 0 {
-	    panic("На ноль делить нельзя!")
-	} else {
-        //fmt.Println("На ноль делить нельзя!")
-        return a / b
-	}
 }
 
 func Subtract(a, b float64) float64 {
@@ -23,32 +16,69 @@ func Multiply(a, b float64) float64 {
 	return a * b
 }
 
-func main() {
-	var operation string
-	var a, b float64
-	fmt.Print("Введите число a")
-	fmt.Scan(&a)
-	fmt.Print("Введите число b")
-	fmt.Scan(&b)
-	fmt.Print("Введите название операции")
-	_, err := fmt.Scan(&operation)
-	if err != nil {
-	    fmt.Println("Error: ", err)
+func Divide(a, b float64) float64 {
+	if b == 0 {
+		panic("На ноль делить нельзя!")
 	}
+	return a / b
+}
 
-    fmt.Println("Вы ввели некорректное число", a, b)
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Ошибка: ", r)
+		}
+		fmt.Println("Программа завершена.")
+	}()
 
-	switch operation {
-	case "Add":
-        fmt.Println(Add(a,b))
-    case "Divide":
-        fmt.Println(Divide(a,b))
-    case "Subtract":
-        fmt.Println(Divide(a,b))
-    case "Multiply":
-        fmt.Println(Multiply(a,b))
-    default:
-        fmt.Println("Error: Неверное название операции")
-    }
-    defer fmt.Println("Программа завершена.")
+	var a, b float64
+	var operation string
+	var result float64
+
+	for {
+		fmt.Print("Введите число a: ")
+		_, err := fmt.Scan(&a)
+		if err != nil {
+			fmt.Println("Ошибка ввода. Пожалуйста, введите число")
+			fmt.Scan()
+			continue
+		}
+
+		fmt.Print("Введите число b: ")
+		_, err = fmt.Scan(&b)
+		if err != nil {
+			fmt.Println("Ошибка ввода. Пожалуйста, введите число")
+			fmt.Scan()
+			continue
+		}
+
+		fmt.Print("Введите операцию (+, -, *, /): ")
+		_, err = fmt.Scan(&operation)
+		if err != nil {
+			fmt.Println("Ошибка ввода. Пожалуйста, введите операцию")
+			fmt.Scan()
+			continue
+		}
+
+		switch operation {
+		case "+":
+			result = Add(a, b)
+		case "-":
+			result = Subtract(a, b)
+		case "*":
+			result = Multiply(a, b)
+		case "/":
+			result = Divide(a, b)
+			if err != nil {
+				fmt.Println("Ошибка:", err)
+				continue
+			}
+		default:
+			fmt.Println("Неверная операция. Пожалуйста, введите +, -, * или /")
+			continue
+		}
+
+		fmt.Println("Результат: ", result)
+		break
+	}
 }
